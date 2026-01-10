fb2168efeb344b517217b1e349c46a71ee115aa3
rodonisi <simon.rodoni@gmail.com>
2026-01-10 13:55:06 +0100

    format

--- lib/riverpod/api/series.dart
+++ lib/riverpod/api/series.dart
@@ -60,7 +60,8 @@ Future<List<SeriesModel>> recentlyUpdated(Ref ref) async {
 
   final seriesModels = res
       .map(
-        (dto) async => await ref.watch(seriesProvider(seriesId: dto.seriesId).future),
+        (dto) async =>
+            await ref.watch(seriesProvider(seriesId: dto.seriesId).future),
       )
       .toList();
   return await Future.wait(seriesModels);
