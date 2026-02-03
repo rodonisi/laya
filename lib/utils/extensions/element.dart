103282ad129061a930577933552d2e1c6451cadc
rodonisi <simon.rodoni@gmail.com>
2026-02-03 20:34:17 +0100

    consider element ids in xpath computation

--- lib/utils/extensions/element.dart
+++ lib/utils/extensions/element.dart
@@ -11,41 +11,53 @@ extension ScrollIdExtension on Element {
     final List<String> paths = [];
     Element? current = this;
 
-    while (current != null && current.localName != null) {
-      final String tagName = current.localName!;
+Element? idParent;
+     while (current != null && current.localName != null) {
+       final String tagName = current.localName!;
 
-      // If it's the root html element, just add it and break
-      if (tagName.toLowerCase() == 'html') {
-        break;
-      }
+       // If it's the root html element, just add it and break
+       if (tagName.toLowerCase() == 'html') {
+         break;
+       }
 
-      // Skip wrapper div with class " "
-      if (tagName.toLowerCase() == 'div' &&
-          current.attributes['class'] == ' ') {
-        current = current.parent;
-        continue;
-      }
+       // Skip wrapper div with class " "
+       if (tagName.toLowerCase() == 'div' &&
+           current.attributes['class'] == ' ') {
+         current = current.parent;
+         continue;
+       }
 
-      // Calculate index among siblings with the same tag name
-      int index = 1;
-      Element? sibling = current.previousElementSibling;
-      while (sibling != null) {
-        if (sibling.localName == tagName) {
-          index++;
-        }
-        sibling = sibling.previousElementSibling;
-      }
+       // Stop at parent with id
+       final String? parentId = current.attributes['id'];
+       if (parentId != null && parentId.isNotEmpty) {
+         idParent = current;
+         break;
+       }
 
-      // Always include index for consistency
-      if (tagName.toLowerCase() == 'body' && index == 1) {
-        paths.add(tagName);
-      } else {
-        paths.add('$tagName[$index]');
-      }
+       // Calculate index among siblings with the same tag name
+       int index = 1;
+       Element? sibling = current.previousElementSibling;
+       while (sibling != null) {
+         if (sibling.localName == tagName) {
+           index++;
+         }
+         sibling = sibling.previousElementSibling;
+       }
 
-      current = current.parent;
-    }
+       // Always include index for consistency
+       if (tagName.toLowerCase() == 'body' && index == 1) {
+         paths.add(tagName);
+       } else {
+         paths.add('$tagName[$index]');
+       }
 
-    return '//${paths.reversed.join('/')}';
+       current = current.parent;
+     }
+
+     final pathString = paths.reversed.join('/');
+     if (idParent != null) {
+       return 'id(${idParent.attributes['id']!})/$pathString';
+     }
+     return '//$pathString';
   }
 }
