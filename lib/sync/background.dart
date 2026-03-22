2551bb0297eee1aab6587cf0ccdf7e639e8ec0d0
rodonisi <simon.rodoni@gmail.com>
2026-03-22 11:18:18 +0100

    expand background sync

--- lib/sync/background.dart
+++ lib/sync/background.dart
@@ -1,10 +1,22 @@
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:kover/api/openapi.swagger.dart';
 import 'package:kover/database/app_database.dart';
+import 'package:kover/riverpod/managers/sync_manager.dart';
 import 'package:kover/riverpod/providers/client.dart';
+import 'package:kover/riverpod/repository/book_repository.dart';
+import 'package:kover/riverpod/repository/chapters_repository.dart';
+import 'package:kover/riverpod/repository/libraries_repository.dart';
 import 'package:kover/riverpod/repository/reader_repository.dart';
+import 'package:kover/riverpod/repository/series_repository.dart';
+import 'package:kover/riverpod/repository/volumes_repository.dart';
+import 'package:kover/riverpod/repository/want_to_read_repository.dart';
+import 'package:kover/sync/book_sync_operations.dart';
+import 'package:kover/sync/chapter_sync_operations.dart';
+import 'package:kover/sync/libraries_sync_operations.dart';
 import 'package:kover/sync/reader_sync_operations.dart';
 import 'package:kover/sync/series_sync_operations.dart';
+import 'package:kover/sync/volume_sync_operations.dart';
+import 'package:kover/sync/want_to_read_sync_operations.dart';
 import 'package:kover/utils/logging.dart';
 import 'package:kover/utils/safe_platform.dart';
 import 'package:workmanager/workmanager.dart';
@@ -19,29 +31,56 @@ void callbackDispatcher() {
       final settings = await db.storageDao.getSettings();
       if (settings == null) return false;
 
-      final chopper = getChopperClient(
-        Uri.parse(settings.url!),
-        settings.apiKey!,
-      );
+      final apiKey = settings.apiKey!;
+      final chopper = getChopperClient(Uri.parse(settings.url!), apiKey);
+      final client = Openapi.create(client: chopper);
 
-      final syncOps = ReaderSyncOperations(
-        client: Openapi.create(client: chopper),
+      final seriesRepo = SeriesRepository(
+        db,
+        SeriesSyncOperations(client: client, apiKey: apiKey),
       );
-
-      final seriesOps = SeriesSyncOperations(
-        client: Openapi.create(client: chopper),
-        apiKey: settings.apiKey!,
+      final bookRepo = BookRepository(
+        db,
+        BookSyncOperations(client: client, apiKey: apiKey),
       );
-
-      final repo = ReaderRepository(
+      final librariesRepo = LibrariesRepository(
         db: db,
-        readerClient: syncOps,
-        seriesClient: seriesOps,
+        client: LibrariesSyncOperations(client),
       );
-      await repo.refreshOutdatedProgress();
-      await repo.mergeProgress();
+      final wantToReadRepo = WantToReadRepository(
+        db,
+        WantToReadSyncOperations(client: client),
+      );
+      final readerRepo = ReaderRepository(
+        db: db,
+        readerClient: ReaderSyncOperations(client: client),
+        seriesClient: SeriesSyncOperations(client: client, apiKey: apiKey),
+      );
+      final volumesRepo = VolumesRepository(
+        db,
+        VolumeSyncOperations(client: client, apiKey: apiKey),
+      );
+      final chaptersRepo = ChaptersRepository(
+        db,
+        ChapterSyncOperations(client: client, apiKey: apiKey),
+      );
 
-      log.d('succesfully merged progress');
+      final engine = SyncEngine(
+        seriesRepo: seriesRepo,
+        bookRepo: bookRepo,
+        librariesRepo: librariesRepo,
+        wantToReadRepo: wantToReadRepo,
+        readerRepo: readerRepo,
+        volumesRepo: volumesRepo,
+        chaptersRepo: chaptersRepo,
+      );
+
+      await engine.syncAllSeries();
+      await engine.syncRecentlyUpdated();
+      await engine.syncRecentlyAdded();
+      await engine.syncLibraries();
+      await engine.syncMetadata();
+      await engine.syncProgress();
 
       return true;
     } catch (e) {
